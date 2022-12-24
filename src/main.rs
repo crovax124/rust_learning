c653d1c33bdde7dbd8cfa167961a7f311862d630
crovax124 <eric.isensee11@gmail.com>
2022-12-24 16:45:21 +0900

    simple mars weight calc, with mut borrowwing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use std::io;
 
 /// A calculator to calculate our weight on Mars
 fn main() {
-
+    println!("Enter your weight (kg): ");
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
-    let mut weight_on_mars:f32 = calculate_our_weight_on_mars(87.0);
+    io::stdin().read_line(&mut input).unwrap();
 
-println!("Weight on Mars: {}kg",weight_on_mars);
+    let weight: f32 = input.trim().parse().unwrap();
+//dbg!(weight);
+    println!("Input: {}", input);
+    let weight_on_mars: f32 = calculate_our_weight_on_mars(weight);
+
+    println!("Weight on Mars: {}kg", weight_on_mars);
 }
 
-fn calculate_our_weight_on_mars(weight:f32) -> f32{
-    (weight/9.81)*3.711
+fn calculate_our_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
